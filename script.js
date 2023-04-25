a6acc6dff3a5c781f7db62197841e0beb29f8071
DanielsZandersons <76948945+DanielsZandersons@users.noreply.github.com>
2023-04-25 14:08:23 +0300

    test0
    test

--- script.js
+++ script.js
@@ -9,14 +9,12 @@ const app = express;
 
 db.run(`CREATE TABLE IF NOT EXISTS meals (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    menuPozicijas_name TEXT NOT NULL,
     menuPozicijas_info TEXT NOT NULL,
     menuPozicijas_cena DECIMAL(5,2) NOT NULL
   )`);
 
 const data = {
-    menuPozicijas_name: 'Crocodile stake',
-    menuPozicijas_info: 'Coocked crocodile from Arizona with butter sauce',
+    menuPozicijas_info: 'Crocodile stake - Coocked crocodile from Arizona with butter sauce',
     menuPozicijas_cena: 9.99
 };
 //b.get(`SELECT * FROM meals', (err, data) => {console.log('data', data)});
